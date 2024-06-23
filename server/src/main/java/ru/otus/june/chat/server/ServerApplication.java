7aad13f8a5aac6352ffbb328c9f594fbd148ada5
Rexice23 <leouniverse@mail.ru>
2024-06-23 15:26:34 +0300

    june.chat initial commit

--- server/src/main/java/ru/otus/june/chat/server/ServerApplication.java
+++ server/src/main/java/ru/otus/june/chat/server/ServerApplication.java
@@ -1,14 +1,7 @@
 package ru.otus.june.chat.server;
 
-import java.net.ServerSocket;
-
 public class ServerApplication {
     public static void main(String[] args) {
-        int port = 8189;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Server(8189).start();
     }
 }
